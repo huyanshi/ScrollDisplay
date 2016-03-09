0e084b738ee750c9b7188d41230c638e24acb3a4
yanshi <yanshi_hu@hiersun.com>
2016-03-09 11:00:12 +0800

    制作轮子

---
+++ ScrollDisplay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScrollDisplay
+//
+//  Created by 胡琰士 on 16/3/9.
+//  Copyright © 2016年 胡琰士. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
