a822c90cf0e7b3efb3dc4b241a1fe94f86508f85
yanshi <yanshi_hu@hiersun.com>
2016-04-08 13:46:57 +0800

    add

--- ScrollDisplay/ViewController.swift
+++ ScrollDisplay/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController,ScrollDisplayViewControllerDelegate,UITableViewDelegate {
 
+    @IBOutlet weak var btn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         let imgNames:[String] = ["1","2","3","4"]
@@ -22,7 +23,43 @@ class ViewController: UIViewController,ScrollDisplayViewControllerDelegate,UITab
             make.right.equalTo(-30)
             make.height.equalTo(200)
         }
-        
+        let label = UILabel()
+        label.text = "  这个测试文本  "
+        label.textColor = UIColor.grayColor()
+        label.font = UIFont.systemFontOfSize(11)
+        label.layer.borderColor = UIColor.blackColor().CGColor
+        label.layer.borderWidth = 1
+        label.layer.cornerRadius = 8
+        view.addSubview(label)
+        label.snp_makeConstraints { (make) -> Void in
+            make.bottom.equalTo(-30)
+            make.left.equalTo(50)
+            make.height.equalTo(24)
+        }
+        /*
+        let watch = imageAndText("icon-24", text: "999")
+        view.addSubview(watch)
+        watch.snp_makeConstraints { (make) -> Void in
+            make.bottom.equalTo(-60)
+            make.left.equalTo(50)
+            make.size.equalTo(CGSize(width: 50, height: 20))
+        }
+        */
+        let imgText = UIButton(type: UIButtonType.Custom)
+        imgText.backgroundColor = UIColor.redColor()
+        btn.setTitle("999", forState: UIControlState.Normal)
+        btn.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        btn.titleLabel?.font = UIFont.systemFontOfSize(10)
+//                imgText.titleEdgeInsets = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: -20)
+        btn.setImage(UIImage(named: "icon-24"), forState: UIControlState.Normal)
+                btn.imageEdgeInsets = UIEdgeInsets(top: 0, left: -5, bottom: 0, right: 5)
+//        self.view.addSubview(btn)
+//        imgText.snp_makeConstraints { (make) -> Void in
+//            make.bottom.equalTo(-60)
+//            make.left.equalTo(50)
+//            make.size.equalTo(CGSize(width: 50, height: 20))
+//        }
+
         // Do any additional setup after loading the view, typically from a nib.
     }
     
@@ -38,7 +75,16 @@ class ViewController: UIViewController,ScrollDisplayViewControllerDelegate,UITab
     func scrollDisplayViewController(scrollDisplayViewController: ScrollDisplayViewController, didSelectedIndex index: NSInteger) {
         print(index)
     }
-    
+    func imageAndText(imageName:String,text:String) -> (UIButton) {
+        let imgText = UIButton(type: UIButtonType.Custom)
+        imgText.setTitle(text, forState: UIControlState.Normal)
+        imgText.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        imgText.titleLabel?.font = UIFont.systemFontOfSize(10)
+//        imgText.titleEdgeInsets = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: -20)
+        imgText.setImage(UIImage(named: imageName), forState: UIControlState.Normal)
+//        imgText.imageEdgeInsets = UIEdgeInsets(top: 0, left: -15, bottom: 0, right: 15)
+        return imgText
+    }
     
 }
 
